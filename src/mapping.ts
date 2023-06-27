7fa765b6bf6baf37e261f58a5ef61784001df7c6
Lior Agnin <lior.agnin@gmail.com>
2023-06-27 16:45:06 +0300

    Merge branch 'master' into graph-updates & fix revert error

--- src/mapping.ts
+++ src/mapping.ts
@@ -30,9 +30,13 @@ export function handleTransfer(event: Transfer): void {
       item.revealed = false;
       item.tokenId = event.params.tokenId;
       item.collection = collection.id;
-      item.descriptorUri = Erc721.bind(event.address).tokenURI(
+      let tokenURIResult = Erc721.bind(event.address).try_tokenURI(
         event.params.tokenId
       );
+      if (tokenURIResult.reverted) {
+        return
+      }
+      item.descriptorUri = tokenURIResult.value
       item.created = event.block.timestamp;
       item.save();
       item = readMetadata(item, item.descriptorUri);
